3aa98e6d0aed6ef79b87f4c85b8c93f7589264b3
Shishir Mahato <mahatoshishir6@gmail.com>
2026-04-29 20:24:20 +0530

    feat: integrate hero, nutrition, and insight sections in Benefits page

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,12 +1,17 @@
 import HealthHeroSection from "./HealthHeroSection";
 import NutritionCardSection from "./NutritionCardSection";
+import ProductInsightCard from "./ProductInsightCard";
+import "./HealthBenefitsSection.css";
 
 const Benefits = () => {
   return (
-    <>
+    <div className="olive-benefits-wrapper">
       <HealthHeroSection />
       <NutritionCardSection />
-    </>
+      <section className="olive-bottom-section">
+        <ProductInsightCard />
+      </section>
+    </div>
   );
 };
 
